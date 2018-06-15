f2751add684cd59353c59a2f37cb82818e4f7aea
itsmenayan <aroranayan@hotmail.com>
2018-06-15 15:46:48 +0530

    File Upload Changes

--- example/src/main/java/com/demo/example/controller/DemoHelloWorld.java
+++ example/src/main/java/com/demo/example/controller/DemoHelloWorld.java
@@ -1,17 +1,25 @@
 package com.demo.example.controller;
 
+
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
 public class DemoHelloWorld {
-	
-	
-	  @RequestMapping("/red")
-	  public String index() {
+
+	  
+	  @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+	  public @ResponseBody ResponseEntity<String> checkTestMapping(@RequestBody MultipartFile file){
+		  System.out.println(file);
+		  return null;
 		  
-	    return "index.html";
 	  }
 
 	}
--- example/src/main/resources/static/js/homeController.js
+++ example/src/main/resources/static/js/homeController.js
@@ -1,6 +1,51 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var myApp = angular.module('myApp', []);
 
-app.controller('homeController', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-});
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+myApp.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+        })
+        .error(function(){
+        });
+    }
+}]);
+
+myApp.controller('myCtrl', ['$scope', 'fileUpload','$http', function($scope, fileUpload,$http){
+    
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+        $http({
+            method : "POST",
+            url : "/Example/fileUpload",
+            data : {'file' : file}
+          }).then(function mySuccess(response) {
+             console.log(response);
+            }, function myError(response) {
+            console.log(response);
+          });
+    };
+    
+}]);
